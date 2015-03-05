325ccb2a4ea47750a9925e72a1a85ec80de6041a
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-03-05 17:45:21 +0100

    Added between_dates to Operation's QuerySet.

--- reports/miner/operations_manager.py
+++ reports/miner/operations_manager.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class OperationQuerySet(models.QuerySet):
-    pass
+    def between_dates(self, upper_date, lower_date):
+        return self.filter(start__lte=upper_date,
+                           start__gte=lower_date)
 
 
 class OperationManager(models.Manager):
