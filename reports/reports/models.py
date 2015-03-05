9da9fbfbf1115d085b06748a024f2369c1e4dbe9
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-03-05 18:04:20 +0100

    Added Report's Manager.

--- reports/reports/models.py
+++ reports/reports/models.py
@@ -1,6 +1,82 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models.query import QuerySet
 
-from miner.models import FrequentItemSet
+from miner.models import FrequentItemSet, Operation
+
+import datetime
+import pytz
+
+
+def _find_same_items(item, operations):
+    frequent_item_sets = []
+    print "operations " + str(len(operations))
+    for operation in operations:
+        print operation
+        sets = operation.item_set.all().filter(
+            generator=False, protocol=item.protocol,
+            remote_host=item.remote_host, remote_port=item.remote_port,
+            local_host=item.local_host, count=item.count,
+            )
+        frequent_item_sets += sets
+    return frequent_item_sets
+
+
+class ReportQuerySet(QuerySet):
+    pass
+
+
+class ReportManager(models.Manager):
+    def get_queryset(self):
+        return ReportQuerySet(self.model, using='default')
+
+    @transaction.atomic
+    def create(self, interval,
+               start_time=datetime.datetime.now(pytz.utc).replace(
+                   minute=0, second=0, microsecond=0), **kwargs):
+        end_time = start_time - datetime.timedelta(hours=interval)
+        kwargs.update({'start_time': start_time, 'end_time': end_time})
+        print "#######"
+        report = super(ReportManager, self).create(**kwargs)
+        print "@@@@@@@"
+        print "start " + str(start_time)
+        print "end " + str(end_time)
+
+        operations = Operation.objects.all().between_dates(
+            upper_date=start_time, lower_date=end_time)
+        frequent_item_sets = FrequentItemSet.objects.filter(
+            operation__in=operations,
+            generator=False)
+        print "freq " + str(len(frequent_item_sets))
+        for item in frequent_item_sets:
+            print item
+            one_hour_occurrences = 0
+            # other_operations = Operation.objects. \
+            #     filter(start__lte=end_time)
+            # itemsets = _find_same_items(
+            #     item=item, operations=other_operations)
+
+            itemsets = FrequentItemSet.objects.filter(
+                generator=False, protocol=item.protocol,
+                remote_host=item.remote_host, remote_port=item.remote_port,
+                local_host=item.local_host, count=item.count,
+                operation__start__lte=end_time)
+            report_item = ReportItem.objects.create(
+                frequent_item_set=item.id, report=report)
+            end_date = Operation.objects.first().end
+            delta = datetime.timedelta(hours=1)
+            date = end_time
+            while date > end_date:
+                date2 = date - delta
+                l = [itemset.is_between_dates(date, date2)
+                     for itemset in itemsets]
+                if any(l):
+                    one_hour_occurrences += 1
+                date -= delta
+            report_item.one_hour_occurrences = one_hour_occurrences
+            print " finished date processing "
+            report_item.save()
+
+        return report
 
 
 class Report(models.Model):
@@ -14,6 +90,8 @@ class Report(models.Model):
     end_time = models.DateTimeField(
         verbose_name="Lower time bracket"
     )
+
+    objects = ReportManager()
 
     def get_occurrences_number(self):
         return len(self.items.all())
